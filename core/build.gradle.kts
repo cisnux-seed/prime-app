abf662252a1055a381e8c2ade073b7a71ebfb37c
Fajra Risqulla <fajrarisqulla@gmail.com>
2023-08-01 10:52:42 +0700

    refactoring code

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -96,19 +96,20 @@ dependencies {
 }
 
 
-ksp {
-    class RoomSchemaArgProvider(
-        @get:InputDirectory
-        @get:PathSensitive(PathSensitivity.RELATIVE)
-        val schemaDir: File
-    ) : CommandLineArgumentProvider {
+class RoomSchemaArgProvider(
+    @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    val schemaDir: File
+) : CommandLineArgumentProvider {
 
-        override fun asArguments(): Iterable<String> {
-            // Note: If you're using KSP, change the line below to return
-            // listOf("room.schemaLocation=${schemaDir.path}").
-            return listOf("room.schemaLocation=${schemaDir.path}")
-        }
+    override fun asArguments(): Iterable<String> {
+        // Note: If you're using KSP, change the line below to return
+        // listOf("room.schemaLocation=${schemaDir.path}").
+        return listOf("room.schemaLocation=${schemaDir.path}")
     }
+}
+
+ksp {
     arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
 }
 
